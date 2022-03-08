68d3eff097a230a9278da537507e3ceaddfcdf2c
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-03-07 22:03:37 -0300

    atvJavaPrimeira

---
+++ 2022-1/EstrtuturaDeDados/atvJava/src/Atv1.java
@@ -0,0 +1,21 @@
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Atv1 {
+
+    public static void main(String[] args) {
+
+         Map<Integer, String> tempSemana = new HashMap<Integer, String>();
+         tempSemana.put(1, "segunda-feira");
+         tempSemana.put(2, "terça-feira");
+         tempSemana.put(3, "quarta-feira");
+         tempSemana.put(4, "quinta-feira");
+         tempSemana.put(5, "sexta-feira");
+         tempSemana.put(6, "sabado");
+         tempSemana.put(7, "domingo");
+
+
+
+    }
+}
