a35408799437797242b118b1a33300d0471a4ddf
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-04-01 19:15:24 -0300

    atvCassio

--- 2022-1/EstrtuturaDeDados/atvJava/src/atividade4.kt
+++ 2022-1/EstrtuturaDeDados/atvJava/src/atividade4.kt
@@ -1,6 +1,4 @@
 fun main () {
-
     var numeros = listOf(10, 5, 4, 3, 2, 90, 32)
     println(numeros.reversed())
-
 }
---
+++ 2022-1/EstrtuturaDeDados/atvJava/src/atividade5.kt
@@ -0,0 +1,15 @@
+fun main () {
+    var vetor = IntArray(50)
+    var xisdead = vetor..149
+
+    //var num = 0
+
+  for (num in vetor){
+      println(xisdead)
+  }
+
+}
+
+private operator fun IntArray.rangeTo(i: Int) {
+
+}
